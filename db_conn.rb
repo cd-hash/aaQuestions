55049ae495abf98ca0d45f84b0338e383c36d139
chris <cdresher2010@gmail.com>
2023-04-23 12:17:03 -0400

    fixed Singleton spelling and ran ruby linter

--- db_conn.rb
+++ db_conn.rb
@@ -1,11 +1,11 @@
-require 'sqlite3'
-require 'singleton'
+require "sqlite3"
+require "singleton"
 
 class QuestionsDBConnection < SQLite3::Database
-  include singleton
+  include Singleton
 
   def initialize
-    super('questions.db')
+    super("questions.db")
     self.type_translations = true
     self.results_as_hash = true
   end
