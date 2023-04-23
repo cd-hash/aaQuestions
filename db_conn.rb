411cc6aa396369ad11d6d4c9fe8fc0c1d8bc80ea
chris <cdresher2010@gmail.com>
2023-04-23 17:03:33 -0400

    fixed spelling of type_translation

--- db_conn.rb
+++ db_conn.rb
@@ -1,12 +1,12 @@
-require "sqlite3"
-require "singleton"
+require 'sqlite3'
+require 'singleton'
 
 class QuestionsDBConnection < SQLite3::Database
   include Singleton
 
   def initialize
-    super("questions.db")
-    self.type_translations = true
+    super('questions.db')
+    self.type_translation = true
     self.results_as_hash = true
   end
 end
