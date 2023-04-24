3f65a631219a866e5bbe9a743f541af48c6e81c5
chris <cdresher2010@gmail.com>
2023-04-24 16:56:01 -0400

    Add ORM class for question_follows table

---
+++ models/question_follows.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require_relative '../db_conn'
+
+# ORM class for QuestionFollows table
+class QuestionFollows
+  attr_reader :user_id, :question_id
+
+  def self.all
+    data = QuestionsDBConnection.instance.execute('SELECT * FROM question_follows')
+    data.map { |datum| QuestionFollows.new(datum) }
+  end
+
+  def self.find_by_id(question_follows_id)
+    data = QuestionsDBConnection.instance.execute(<<-SQL, question_follows_id: question_follows_id)
+      SELECT *
+      FROM question_follows
+      WHERE question_follows.id=:question_follows_id
+    SQL
+    data.map { |datum| QuestionFollows.new(datum) }
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @user_id = options['user_id']
+    @question_id = options['question_id']
+  end
+end
